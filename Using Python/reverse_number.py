84a6e0b4efcd8c20a4f477569f57bf4cb667c97b
ANUJ BISHT <anujbisht09@gmail.com>
2023-04-24 08:44:19 +0530

    Update reverse_number.py

--- Using Python/reverse_number.py
+++ Using Python/reverse_number.py
@@ -1,1 +1,33 @@
+# Program to print the reverse of the number
+
+# function to reverse the number
+def reverse_number(nums: int) -> int:
+    # convert the nums to string 
+    nums_str = str(nums)
+    if nums >= 0:
+        # reverse the string and convert it to integer
+        reverse_nums = int(nums_str[::-1])
+        if reverse_nums <= 2147483647:
+            return reverse_nums
+        else:
+            return 0
+    else:
+        # remove the "-" sign from the string
+        nums_str_new = nums_str.replace("-", "")
+        # reverse the string and convert it to integer
+        reverse_nums_str_new = int(nums_str_new[::-1])
+        # putting negative sign in the beginning of number
+        reverse_nums = -reverse_nums_str_new
+        if reverse_nums >= -2147483648:
+            return reverse_nums
+        else:
+            return 0
+
+
+# driver function
+if __name__ == "__main__":
+    # scan the number
+    number = int(input("Enter a number: "))
+    # print the reverse of the number
+    print(f"Reverse Number: {reverse_number(number)}")
 
